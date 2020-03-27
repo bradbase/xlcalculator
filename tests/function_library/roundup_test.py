88fb5ef69a4ccf033cf68e576f95fd4e5f546d63
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-03-27 23:32:29 +1100

    Improve testing for function ROUNDUP with eval
    Co-Authored-By: Bradley van Ree <bradbase@users.noreply.github.com>

--- tests/function_library/roundup_test.py
+++ tests/function_library/roundup_test.py
@@ -1,13 +1,24 @@
 
+# Excel reference: https://support.office.com/en-us/article/roundup-function-f8bc9b23-e795-47db-8703-db171d0c42a7
+
 import unittest
 
 import pandas as pd
 
 from koala_xlcalculator.function_library import xRound
 from koala_xlcalculator.exceptions import ExcelError
-
+from koala_xlcalculator import ModelCompiler
+from koala_xlcalculator import Evaluator
 
 class Test_Roundup(unittest.TestCase):
+
+    def setUp(self):
+        compiler = ModelCompiler()
+        self.model = compiler.read_and_parse_archive(r"./tests/resources/ROUNDUP.xlsx")
+        self.model.build_code()
+        self.evaluator = Evaluator(self.model)
+
+
     def test_nb_must_be_number(self):
         with self.assertRaises(ExcelError):
             xRound.roundup('er', 1)
@@ -30,3 +41,35 @@ class Test_Roundup(unittest.TestCase):
         self.assertEqual(xRound.roundup(76.9,0), 77)
         self.assertEqual(xRound.roundup(3.14159, 3), 3.142)
         self.assertEqual(xRound.roundup(-3.14159, 1), -3.2)
+
+
+    def test_evaluation_A1(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A1')
+        value = self.evaluator.evaluate('Sheet1!A1')
+        self.assertEqual( excel_value, value )
+
+
+    def test_evaluation_A2(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A2')
+        value = self.evaluator.evaluate('Sheet1!A2')
+        self.assertEqual( excel_value, value )
+
+
+    def test_evaluation_A3(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A3')
+        value = self.evaluator.evaluate('Sheet1!A3')
+        self.assertEqual( excel_value, value )
+
+
+    @unittest.skip('Problem evalling: #VALUE! for Sheet1!A4, xRound.roundup(Evaluator.apply_one("minus", 3.14159, None, None),1)')
+    def test_evaluation_A4(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A4')
+        value = self.evaluator.evaluate('Sheet1!A4')
+        self.assertEqual( excel_value, value )
+
+
+    @unittest.skip('Problem evalling: #VALUE! for Sheet1!A5, xRound.roundup(31415.92654,Evaluator.apply_one("minus", 2, None, None))')
+    def test_evaluation_A5(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A5')
+        value = self.evaluator.evaluate('Sheet1!A5')
+        self.assertEqual( excel_value, value )
