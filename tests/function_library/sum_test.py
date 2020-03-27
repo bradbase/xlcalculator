b6ed3121e7078dc10362de3bf441bc5e279784dd
Bradley van Ree <38775942+Brad-eki@users.noreply.github.com>
2020-03-27 23:37:44 +1100

    Improving testing for function SUM with eval
    Co-Authored-By: Bradley van Ree <bradbase@users.noreply.github.com>

--- tests/function_library/sum_test.py
+++ tests/function_library/sum_test.py
@@ -1,15 +1,23 @@
+
+# Excel reference: https://support.office.com/en-us/article/sum-function-043e1c7d-7726-4e80-8f32-07b23e057f89
+
 import unittest
 
 import pandas as pd
 
 from koala_xlcalculator.function_library import xSum
 from koala_xlcalculator.koala_types import XLRange
+from koala_xlcalculator import ModelCompiler
+from koala_xlcalculator import Evaluator
 
 
 class TestSum(unittest.TestCase):
 
-    # def setUp(self):
-    #     pass
+    def setUp(self):
+        compiler = ModelCompiler()
+        self.model = compiler.read_and_parse_archive(r"./tests/resources/SUM.xlsx")
+        self.model.build_code()
+        self.evaluator = Evaluator(self.model)
 
     # def teardown(self):
     #     pass
@@ -24,3 +32,9 @@ class TestSum(unittest.TestCase):
         xsum_result_01 = xSum.xsum(range_01)
         result_01 = 10
         self.assertEqual(result_01, xsum_result_01)
+
+
+    def test_evaluation_A10(self):
+        excel_value = self.evaluator.get_cell_value('Sheet1!A10')
+        value = self.evaluator.evaluate('Sheet1!A10')
+        self.assertEqual( excel_value, value )
