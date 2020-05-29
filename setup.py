990cd91a90f0f523d9f5a21de5c806494a217fae
Stephan Richter <stephan.richter@gmail.com>
2020-05-28 22:46:14 -0400

    Back to development: 0.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ TESTS_REQUIRE = [
 
 setuptools.setup(
     name="xlcalculator",
-    version='0.2.2',
+    version='0.2.3.dev0',
     author="Bradley van Ree",
     author_email="brads@bradbase.net",
     description="Converts MS Excel formulas to Python and evaluates them.",
