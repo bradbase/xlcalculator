5f03296cfc15b5e500713f5f54bb0e396c50b124
Bradley van Ree <47906258+bradbase@users.noreply.github.com>
2020-11-28 11:46:47 +1100

    Roll version number
    Co-Authored-By: Bradley van Ree <47906258+bradbase@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ TESTS_REQUIRE = [
 
 setuptools.setup(
     name="xlcalculator",
-    version='0.2.12.dev',
+    version='0.2.13.dev',
     author="Bradley van Ree",
     author_email="brads@bradbase.net",
     description="Converts MS Excel formulas to Python and evaluates them.",
