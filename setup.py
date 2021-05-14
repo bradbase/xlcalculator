a1014e10ee45b425a9618f58edf44533e0fc4ab2
Stephan Richter <stephan.richter@gmail.com>
2021-05-14 06:10:12 -0400

    Preparing release 0.4.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ TESTS_REQUIRE = [
 
 setuptools.setup(
     name="xlcalculator",
-    version='0.4.1.dev0',
+    version='0.4.1',
     author="Bradley van Ree",
     author_email="brads@bradbase.net",
     description="Converts MS Excel formulas to Python and evaluates them.",
